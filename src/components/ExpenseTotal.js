0037a90ae9444c4d8d40fc5b5a6d7d488ae35a7e
Prakash Chandra Pant <prakashgbpecmca@outlook.com>
2024-05-22 16:29:23 +0530

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,21 @@
 
+
+import React, {useContext} from 'react';
+import { AppContext } from '../context/AppContext';
+
+const ExpenseTotal = () => {
+
+    const {expenses} = useContext(AppContext);
+    const totalExpense = expenses.reduce((total,item) => {
+        return (total += item.cost)
+    },0);
+
+    return (
+        <div className='alert alert-primary'>
+        <span> Spent so far: ${totalExpense}</span>
+        </div>
+
+    );
+}
+
+export default ExpenseTotal;
