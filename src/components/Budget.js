d4e3e4fbef20f701b63257e4ee6a01abf7548576
Prakash Chandra Pant <prakashgbpecmca@outlook.com>
2024-05-22 16:28:36 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,22 @@
 
+
+import React, {useContext, useState} from 'react';
+import  {AppContext} from '../context/AppContext';
+
+const Budget = () => {
+
+    const {budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handelBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+
+    return (<div className='alert alert-secondary'>
+        <span>Bedget: ${budget}</span>
+        <input type='number' step="10" 
+        value={newBudget} onChange={handelBudgetChange}/>
+    </div>);
+}
+
+export default Budget;
