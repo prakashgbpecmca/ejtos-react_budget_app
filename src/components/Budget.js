a749a0e41c79ee09423f91c3a8cad5838313e1ca
Prakash Chandra Pant <prakashgbpecmca@outlook.com>
2024-06-03 00:08:53 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,15 +5,24 @@ import  {AppContext} from '../context/AppContext';
 
 const Budget = () => {
 
-    const {budget } = useContext(AppContext);
+    const {budget,remaining,currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
 
     const handelBudgetChange = (event) => {
+        if (event.target.value> 20000) {
+            alert("value cannot exceed 20000");
+            return;
+        }
+
+        if(event.target.value < remaining) {
+            alert("You cannot reduce the budget value lower than the spending");
+            return;
+        }
         setNewBudget(event.target.value);
     }
 
     return (<div className='alert alert-secondary'>
-        <span>Bedget: ${budget}</span>
+        <span>Budget: {currency}</span>
         <input type='number' step="10" 
         value={newBudget} onChange={handelBudgetChange}/>
     </div>);
